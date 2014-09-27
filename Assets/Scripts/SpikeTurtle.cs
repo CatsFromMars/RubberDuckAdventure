199be288690395e4d9d7ae743fddb60207212f33
Jason Kim <jason.gwkim@gmail.com>
2014-09-27 16:54:54 -0400

    Added Spike Turtle

---
+++ Assets/Scripts/SpikeTurtle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeTurtle: MonoBehaviour {
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            //other.gameObject.health -= 1 should be added when Player is more clearly defined.
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
