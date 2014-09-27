183307551bbcf92f9cbf03c2dca31ff04e0e5661
CatsFromMars <mduque@andrew.cmu.edu>
2014-09-27 16:12:25 -0400

    Project Organize

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+	public float velocityX = 1f;
+	public float velocityY = 0f;
+	float velocityXLimit = 3f;
+	float velocityYLimit = 5f;
+	float accelerationX = 0.1f;
+	float accelerationY = 3f;
+	int damage_level;
+
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	
+	}
+
+	void FixedUpdate() {
+		GetInput();
+
+	}
+
+	void GetInput() {
+		if (Input.GetButton("Whirlpool"))
+		{
+			velocityX = 0;
+		}
+
+	}
+
+	void Move() {
+		//MOVE TO THE RIGHT
+		transform.Translate(Time.deltaTime*velocityX, Time.deltaTime*velocityY, 0);
+
+		//ACCELTERATE
+		if(velocityX < velocityXLimit) velocityX += accelerationX;
+	}
+}
