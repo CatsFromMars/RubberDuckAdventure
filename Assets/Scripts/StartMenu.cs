d294f516aef8a4e733866a3a9b127835b219e6ff
Evan Lee <leafkonoha42@gmail.com>
2014-10-04 15:04:41 -0400

    Very bare title menu with placeholder image.

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+	private int width, height;
+	private int boxWidth, boxHeight;
+
+	public bool visible = true;
+
+	// Use this for initialization
+	void Start () {
+		width = Screen.width;
+		height = Screen.height;
+		boxWidth = 400;
+		boxHeight = 200;
+	} 
+	
+	// Update is called once per frame
+	void OnGUI () {
+		if (visible) {
+			GUILayout.BeginArea (new Rect((width - boxWidth) / 2, (height + boxHeight) / 2, boxWidth, boxHeight));
+			GUILayout.BeginVertical ();
+			if (GUILayout.Button ("Play")) {
+				Application.LoadLevel("Test");
+			}
+			if (GUILayout.Button ("Quit")) {
+				Application.Quit();
+			}
+			GUILayout.EndVertical ();
+			GUILayout.EndArea ();
+		}
+	}
+}
