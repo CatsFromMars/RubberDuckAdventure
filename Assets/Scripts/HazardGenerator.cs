6e54dd6e2886a01891ed689824af4459acf9c86a
Evan Lee <leafkonoha42@gmail.com>
2014-10-04 17:00:17 -0400

    Started Generator script

---
+++ Assets/Scripts/HazardGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HazardGenerator : MonoBehaviour {
+
+	public List<GameObject> hazards;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
