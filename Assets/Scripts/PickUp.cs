e7c579252b2232565cdbfa5176a168229c8e61fc
apleshak <pleshakov.anton@gmail.com>
2014-11-22 15:24:19 -0500

    hats implemented

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -4,16 +4,17 @@ using System.Collections;
 public class PickUp : MonoBehaviour {
 
     GameObject player;
+    Texture hatTexture;
 
 	// Use this for initialization
 	void Start () 
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        hatTexture = this.gameObject.GetComponent<MeshRenderer>().materials[0].GetTexture(0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
     void OnTriggerEnter(Collider other)
@@ -21,9 +22,12 @@ public class PickUp : MonoBehaviour {
         if (other.gameObject.tag == "Player")
         {
             Transform hat = player.transform.FindChild("DuckHatLayer");
+            Debug.Log(hat.tag);
             MeshRenderer mesh = hat.gameObject.GetComponent<MeshRenderer>();
-            Texture texture = mesh.material.GetTexture(0);
-            texture = this.gameObject.GetComponent<MeshRenderer>().material.GetTexture(0);
+
+            hat.renderer.sharedMaterial.SetTexture(0,hatTexture);
+
+            //texture = this.gameObject.GetComponent<MeshRenderer>().material.GetTexture(0);
             Destroy(this.gameObject);
         }
     }
