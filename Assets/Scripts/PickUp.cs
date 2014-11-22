1847622163f77586bd3c2e53871b7a5fd1a4b14d
apleshak <pleshakov.anton@gmail.com>
2014-11-22 14:54:54 -0500

    hat script called PickUp

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () 
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Transform hat = player.transform.FindChild("DuckHatLayer");
+            MeshRenderer mesh = hat.gameObject.GetComponent<MeshRenderer>();
+            Texture texture = mesh.material.GetTexture(0);
+            texture = this.gameObject.GetComponent<MeshRenderer>().material.GetTexture(0);
+            Destroy(this.gameObject);
+        }
+    }
+}
